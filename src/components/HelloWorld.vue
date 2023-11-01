ae91fee5c643e6939394ff75f7e327cfec121ddb
Ross Coundon <ross.coundon@leadentsolutions.com>
2023-11-01 14:41:42 +0000

    Add button to request permissions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
+
+import { computed } from 'vue';
+
 defineProps<{
   msg: string
 }>()
+
+function requestNotificationPermission() {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  } else {
+    Notification.requestPermission();
+  }
+}
+
+const notificationPermission = computed(() => {
+  return Notification.permission === 'granted'
+});
+
+
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <button v-if="!notificationPermission" @click="requestNotificationPermission">Keep me up-to-date</button>
     </h3>
   </div>
 </template>
